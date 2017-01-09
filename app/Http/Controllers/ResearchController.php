36027f30f8a8845375ebc27d59b924ab659b0bde
vetostream <genesisbentulan@gmail.com>
2017-01-10 03:55:50 +0800

    Research Module

---
+++ app/Http/Controllers/ResearchController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Research;
+use App\Http\Requests\StoreResearch;
+
+
+class ResearchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get all research
+        $researches = Research::all();
+        return view('research.index')->with('researches', $researches);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('research.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreResearch $request)
+    {
+        $research = new Research;
+
+        $research->title = $request->input('title');
+        $research->research_abstract = $request->input('research_abstract');
+        $research->save();
+
+        return redirect('/research');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try 
+        {
+            $research = Research::findOrFail($id);
+            return view('research.detail')->with('research', $research);
+        }
+        catch(\Exception $e)
+        {
+            abort(404,"Research ID not Found!");
+        }
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        try
+        {
+            $research = Research::findOrFail($id);
+            return view('research.edit')->with('research', $research);
+        }
+        catch(\Exception $e){
+            abort(404,"Research ID not Found!");
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreResearch $request, $id)
+    {
+        try{
+            $research = Research::findOrFail($id);
+            $research->title = $request->input('title');
+            $research->research_abstract = $request->input('research_abstract');
+            $research->save();
+
+            return view('research.detail')->with('research',$research);
+        }catch(\Exception $e){
+            abort(404, $e);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+    
+        try
+        {
+            $research = Research::findOrFail($id);
+            $research->delete();
+        }
+        catch(\Exception $e){
+            abort(404, $e);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+//Authentication Routes
 Auth::routes();
 
+
 Route::get('/home', 'HomeController@index');
+
+
+//Research routes
+Route::get('/research','ResearchController@index'); //show list of exhibited researches
+Route::get('research/detail/{id}','ResearchController@show'); //show details of the chosen research by id
+Route::get('/research/create','ResearchController@create'); //show form for creating Research
+Route::post('/research/store','ResearchController@store'); //store newly created resource
+Route::get('/research/edit/{id}','ResearchController@edit'); //show form to edit exhibited research
+Route::post('/research/update/{id}','ResearchController@update'); //update edited researches
+Route::get('/research/exterminate/{id}','ResearchController@destroy'); //destroy the resource of the id indicated.
