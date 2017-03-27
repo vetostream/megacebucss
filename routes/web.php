f1bce63882e833c80eee8a51bef1d82358373168
vetostream <genesisbentulan@gmail.com>
2017-03-27 19:37:15 +0800

    activation table migration

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 //Authentication Routes
 Auth::routes();
+Route::get('user/activation/{token}','Auth\AuthController@activateUser')->name('user.activate');
 
 
 Route::get('/home', 'PostController@index');
