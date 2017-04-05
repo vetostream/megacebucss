5bd2aeac632e9926c678d52d7bb920e85bcb074b
vetostream <lennartnat@live.com>
2017-04-05 13:24:17 +0800

    Removed separators

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,14 +13,15 @@
 		<img src="{{ asset('images/fire-african.jpg') }}" style="display: block; transform: translate3d(50%, 271px, 0px);">
 	</div>
 </div> -->
-<div class='separator-black'></div>
+<!-- <div class='separator-black'></div> -->
 <div class="row row-no-margin home-wrapper">
 	<div class="center home-bg-1" style="background-image: url('{{ asset("images/home-ideas.png") }}');">
 		<!-- <h3 class="grey-text text-darken-3"><b>Have an idea? Share it here!</b></h3> -->
 		<a href='register' class='waves-effect waves-light btn btn-jumbo amber accent-3'>Share Your Idea <!-- 	 --></a>
+		<a href='register' class='waves-effect waves-light btn btn-jumbo blue accent-3'>Browse Ideas <!-- 	 --></a>
 	</div>
 </div>
-<div class='separator-black'></div>
+<!-- <div class='separator-black'></div> -->
 <!-- 
 <div class="parallax-container">
 	<div class="parallax">
